557777b72632414736e7190539d70b3477d93cef
sukanyamuthkuru <sukanyamuthkuru46@gmail.com>
2025-01-03 16:59:34 +0000

    update 6

--- src/Components/JobDetailsRoute/index.js
+++ src/Components/JobDetailsRoute/index.js
@@ -1,7 +1,11 @@
 import {Component} from 'react'
 import {Redirect} from 'react-router-dom'
 import Cookies from 'js-cookie'
+import {BsStarFill} from 'react-icons/bs'
+import {MdPlace, MdWork} from 'react-icons/md'
+import {FaExternalLinkAlt} from 'react-icons/fa'
 import Header from '../Header'
+import JobDetailsSkills from '../JobDetailsSkills'
 import './index.css'
 
 const apiStatusConstants = {
@@ -71,9 +75,85 @@ class JobDetailsRoute extends Component {
       const updatedSimilarJobsData = data.similar_jobs.map(each =>
         this.getFormattedSimilarJobsData(each),
       )
-      console.log(updatedSimilarJobsData)
+
+      this.setState({
+        details: updatedDataJobData,
+        similarJobs: updatedSimilarJobsData,
+        apiStatus: apiStatusConstants.success,
+      })
     } catch {
-      console.log('kkkk')
+      this.setState({apiStatus: apiStatusConstants.failure})
+    }
+  }
+
+  renderjobDetailsSuccessView = () => {
+    const {details} = this.state
+    console.log(details)
+    return (
+      <div className="details-container">
+        <div className="job-details-logo-rating-role-container">
+          <img
+            src={details.companyLogoUrl}
+            alt={details.id}
+            className="job-details-company-logo"
+          />
+          <div className="job-details-role-rating-container">
+            <p className="job-detail-role-heading">{details.title}</p>
+            <div className="job-detail-rating-container">
+              <BsStarFill className="star-rating" />
+              <p className="rating-text-job-detail">{details.rating}</p>
+            </div>
+          </div>
+        </div>
+        <div className="job-detail-location-type-ctc-continer">
+          <div className="job-detail-location-type-container">
+            <div className="job-detail-location-logo-container">
+              <MdPlace className="job-detail-location-logo" />
+              <p className="job-detail-location-text">{details.location}</p>
+            </div>
+            <div className="job-detail-location-logo-container">
+              <MdWork className="job-detail-location-logo" />
+              <p className="job-detail-location-text">
+                {details.employmentType}
+              </p>
+            </div>
+          </div>
+          <p className="job-detail-ctc">{details.packagePerAnnum}</p>
+        </div>
+        <hr className="job-detail-horizontal-rural" />
+        <div className="job-detail-description-visit-container">
+          <p className="job-details-description-text">Description</p>
+          <a
+            href={details.companyWebsiteUrl}
+            className="job-detail-anchor-text"
+          >
+            Visit
+            <span>
+              <FaExternalLinkAlt className="link" />
+            </span>
+          </a>
+        </div>
+        <p className="job-detail-description">{details.jobDescription}</p>
+        <p className="job-details-description-text">Skills</p>
+
+        <ul className="job-detail-skills-container">
+          {details.skills.map(each => (
+            <JobDetailsSkills key={each.name} skill={each} />
+          ))}
+        </ul>
+      </div>
+    )
+  }
+
+  renderjobDetails = () => {
+    const {apiStatus} = this.state
+
+    switch (apiStatus) {
+      case apiStatusConstants.success:
+        return this.renderjobDetailsSuccessView()
+
+      default:
+        return null
     }
   }
 
@@ -86,13 +166,7 @@ class JobDetailsRoute extends Component {
     return (
       <div className="jobs-details-route-container">
         <Header />
-        <div className="job-details-container">
-          <div className="details-container">
-            <div className="job-details-logo-rating-role-container">
-              <h1>hiii</h1>
-            </div>
-          </div>
-        </div>
+        <div className="job-details-container">{this.renderjobDetails()}</div>
       </div>
     )
   }
---
+++ src/Components/JobDetailsSkills/index.js
@@ -0,0 +1,13 @@
+import './index.css'
+
+const JobDetailsSkills = props => {
+  const {skill} = props
+
+  return (
+    <li className="skill-list-item">
+      <h1>hii</h1>
+    </li>
+  )
+}
+
+export default JobDetailsSkills
